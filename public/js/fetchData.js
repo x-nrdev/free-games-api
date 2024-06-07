6583055a6352972859c3351665f4c829f0320c27
NrDev <nelson_rojas_janda@hotmail.com>
2024-06-07 17:50:24 +0200

    Add pagination functionality

--- public/js/fetchData.js
+++ public/js/fetchData.js
@@ -6,22 +6,30 @@ import gameCard from './gameCard.js'
 const fetchData = async (tag = 'battle-royale') => {
     ulElem.innerHTML = ''
     paginationElement.innerHTML = ''
-
+    const url = `/api?tag=${tag}`
+    let data = []
     updateLoadingState(true)
 
-    const url = `/api?tag=${tag}`
-    const response = await fetch(url)
-    const data = await response.json()
+    try {
+        const response = await fetch(url)
+        data = await response.json()
+    } catch (err) {
+        console.error(err)
+        return
+    }
 
     // Pagination
-    const pagination = (games, page = 0) => {
+    const pagination = (games, page = 1) => {
+        if (games.length === 0) return
+        if (Number.isNaN(page)) return
+
         const gamesPerPage = 12 // Number of games per page
         const delayMultiplier = 100
         const totalGames = games.length
         const totalPages = Math.ceil(totalGames / gamesPerPage) // Calculate total pages
 
-        const startIndex = page * gamesPerPage
-        const endIndex = (page * gamesPerPage) + gamesPerPage
+        const startIndex = (page - 1) * gamesPerPage
+        const endIndex = page * gamesPerPage
         games = games.slice(startIndex, endIndex)
 
         // Insert fetched data to the DOM
@@ -39,23 +47,42 @@ const fetchData = async (tag = 'battle-royale') => {
 
         // Insert pagination to the DOM
         const setPagination = () => {
+            // Insert pagination buttons
             let ctx = ''
             for (let i = 0; i < totalPages; i++) {
-                const page = i + 1
+                const btnPage = i + 1
+                const isActive = btnPage === page ? 'active' : ''
+
                 ctx += `
-                    <li class="pagination-item}">
-                        <button class="pagination-button" data-page="${page}">${page}</button>
+                    <li class="pagination-item">
+                        <button 
+                            class="fn-pagination ${isActive}"
+                            data-page="${btnPage}"
+                        >
+                            ${btnPage}
+                        </button>
                     </li>
                 `
             }
             paginationElement.innerHTML = ctx
+
+            // Add event listeners to pagination buttons
+            paginationElement.querySelectorAll('button').forEach(btn => {
+                btn.addEventListener('click', e => {
+                    e.preventDefault()
+                    if (e.target.classList.contains('active')) return
+                    const page = +e.target.dataset.page
+                    pagination(data, page)
+                })
+            })
         }
 
         setData()
+        setPagination()
         updateLoadingState(false)
     }
 
-    pagination(data)
+    pagination(data, 1)
 }
 
 export default fetchData
