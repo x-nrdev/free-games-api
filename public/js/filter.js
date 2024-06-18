a96350495acb8b5ea1f0b6217fea353f2bab7e12
nr_dev <nelson_rojas_janda@hotmail.com>
2024-06-18 22:40:32 +0200

    Add delay to the filter if needed

--- public/js/filter.js
+++ public/js/filter.js
@@ -3,54 +3,76 @@ import { updateLoadingState } from "./utils.js"
 
 const filter = async (data, pagination, sortByOption) => {
     const filter = document.querySelector('form.filter')
+    const timeToSearchGame = 0 // Seconds
     let url = `/filter?sort-by=${sortByOption}`
+    let findFilterGamesTimeoutID = 0
+
 
     const fetchData = async (formatedUrl) => {
         try {
             const response = await fetch(formatedUrl)
-            return await response.json()
+            const data = await response.json()
+
+            if (data.status === 0) {
+                //TODO: Clear the filter
+
+                //TODO: Show error message to the user on the client
+                throw new Error('No games found with the given filters')
+            }
+
+            return data
+
         } catch (err) {
             console.error(err)
             return
         }
     }
 
+    const findFilterGames = () => {
+        return setTimeout(async () => {
+            paginationElement.innerHTML = ''
+            ulElem.innerHTML = ''
+            let tags = []
+            let genres = ''
+
+            // Check for filters on the form()
+            for (const input of filter) {
+                const checked = input.checked
+                if (checked) {
+                    const { value, name } = input
+                    if (name === 'platform') {
+                        url += `&platform=${value}`
+                    } else if (name === 'genre') {
+                        tags.push(value)
+                    }
+                }
+            }
+
+            if (tags) {
+                // Format tags for query string
+                genres = tags.join('.')
+                // Add genres to url
+                url += `&tag=${genres}`
+            }
+
+            if ((Array.isArray(tags) && tags.length > 0) || genres) {
+                // Fetch games data
+                const games = await fetchData(url)
+                pagination(games)
+            } else {
+                pagination(data)
+            }
+            url = `/filter?sort-by=${sortByOption}`
+        }, timeToSearchGame * 1000)
+    }
+
     filter.addEventListener('change', async (e) => {
         e.preventDefault()
         updateLoadingState(true)
-        paginationElement.innerHTML = ''
-        ulElem.innerHTML = ''
-        let tags = []
-        let genres = ''
 
-        // Check for filters on the form
-        for (const input of filter) {
-            const checked = input.checked
-            if (checked) {
-                const { value, name } = input
-                if (name === 'platform') {
-                    url += `&platform=${value}`
-                } else if (name === 'genre') {
-                    tags.push(value)
-                }
-            }
-        }
+        clearTimeout(findFilterGamesTimeoutID)
+        findFilterGamesTimeoutID = findFilterGames()
 
-        if (tags) {
-            // Format tags for query string
-            genres = tags.join('.')
-            // Add genres to url
-            url += `&tag=${genres}`
-        }
-
-        if ((Array.isArray(tags) && tags.length > 0) || genres) {
-            // Fetch games data
-            const games = await fetchData(url)
-            pagination(games)
-        } else {
-            pagination(data)
-        }
-        url = `/filter?sort-by=${sortByOption}`
         updateLoadingState(false)
     })
 }
