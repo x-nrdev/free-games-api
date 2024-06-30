369837318b590a1357883538b881cbb822195dbd
NrDev <nelson_rojas_janda@hotmail.com>
2024-06-30 18:24:42 +0200

    Fix blur bug when changing between mobile and desktop

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,6 +1,5 @@
 // Blur page when menu is opened
 export const updatePageBlur = () => {
-    if (window.innerWidth > 940) return
     const main = document.querySelector('main')
     const footer = document.querySelector('footer')
 
