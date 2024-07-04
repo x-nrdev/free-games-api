4f8fb0549b25dd3a2a9f7312f8b0c311e19494ee
NrDev <nelson_rojas_janda@hotmail.com>
2024-07-04 19:04:42 +0200

    Add game genre to game cards

--- public/js/gameCard.js
+++ public/js/gameCard.js
@@ -3,15 +3,17 @@ const gameCard = (game, delay = 0) => {
         title,
         thumbnail,
         short_description,
-        game_url
+        game_url,
+        genre
     } = game
     return `<li class='thumbnail' style='animation-delay: ${delay}ms'>
                 <div class="card">
                     <h3>${title}</h3>
                     <a href='${game_url}' target='_blank'>
-                        <img src="${thumbnail}" alt="${title}" width='256' height='144'>
+                        <img class="card-img" src="${thumbnail}" alt="${title}" width='256' height='144'>
                     </a>
-                    <p>${short_description}</p>
+                    <p class="card-description">${short_description}</p>
+                    <span class="card-genre">${genre}</span>
                 </div>
             </li>`
 }
