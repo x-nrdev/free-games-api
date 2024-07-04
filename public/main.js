2bfe971f467f263f5a06874bd4654f79dfd20afd
NrDev <nelson_rojas_janda@hotmail.com>
2024-07-04 19:41:37 +0200

    Add scroll to top button when user scroll to the bottom

--- public/main.js
+++ public/main.js
@@ -13,6 +13,19 @@ const app = () => {
     navButtons()
     updateScrollStatus()
     search()
+
+    window.addEventListener('scroll', () => {
+        const scrollPosition = window.scrollY || window.pageYOffset
+        const scrollBtn = document.querySelector('.scroll-up-btn')
+        if (scrollPosition < 300) {
+            scrollBtn.classList.remove('active')
+            return
+        }
+        scrollBtn.classList.add('active')
+        scrollBtn.addEventListener('click', () => {
+            globalThis.scrollTo({ top: 0, behavior: 'smooth' })
+        })
+    })
 }
 
 app()
