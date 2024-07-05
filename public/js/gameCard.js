f4189641e17cb4f3cd29294a5bff99a0d941daca
Nelson Rojas <nelson.rojas@playtech.com>
2024-07-05 13:44:12 +0200

    Move h3 of game card after the game image

--- public/js/gameCard.js
+++ public/js/gameCard.js
@@ -8,10 +8,10 @@ const gameCard = (game, delay = 0) => {
     } = game
     return `<li class='thumbnail' style='animation-delay: ${delay}ms'>
                 <div class="card">
-                    <h3>${title}</h3>
                     <a href='${game_url}' target='_blank'>
                         <img class="card-img" src="${thumbnail}" alt="${title}" width='256' height='144'>
                     </a>
+                    <h3>${title}</h3>
                     <p class="card-description">${short_description}</p>
                     <span class="card-genre">${genre}</span>
                 </div>
